8a73ca8ea8b4919f5d98f0b1772bef0e97ba5adf
Gerishon <mwaniki31@gmail.com>
2018-09-03 16:33:29 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function mouseOver() {
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("name").style.color= "pink";
+}
+
+function mouseOut() {
+    document.getElementById("demo").style.color = "green";
+    document.getElementById("name").style.color = "yellow";
+}
+
+
+function big(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+}
+
+function normalImg(x) {
+    x.style.height = "120px";
+    x.style.width = "120px";
+}
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+    
